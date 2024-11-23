19cae34d87259a11e6d41c2a742cc2feeba9bdf4
Run Wang <Raymond1993WR@gmail.com>
2024-11-23 13:30:32 +0000

    feature(botController): add bot enpoint to get list or one bot by id

--- controllers/BotController.js
+++ controllers/BotController.js
@@ -1,7 +1,18 @@
+import validator from 'validator';
 import Bot from '../models/BotModel.js';
 import Worker from '../models/WorkerModel.js';
 
+// TODO: Need to move to ulti folder with test
+const { isUUID } = validator;
+
 const listAllBots = (req, res) => {
+    Bot.findAll().then(bot => {
+        return res.json(bot);
+    });
+}
+
+// query all bots with their workers(future use)
+const listAllBotsWithWorkers = (req, res) => {
     Bot.findAll({
         include: {
             model: Worker,
@@ -12,4 +23,23 @@ const listAllBots = (req, res) => {
     });
 }
 
-export default { listAllBots };
+const findBotById = (req, res) => {
+    const { id } =  req.params;
+
+    // Validate the UUID format
+    if (!isUUID(id)) {
+        return res.status(400).json({ error: 'Invalid UUID format.' });
+    }
+
+    const bot = Bot.findOne({
+        where: { id },
+    });
+
+    if (!bot) {
+        console.log(`Bot with ID ${id} not found.`);
+    }
+
+    return res.status(200).json(bot);
+}
+
+export default { listAllBots, listAllBotsWithWorkers, findBotById };
--- routes/BotRoutes.js
+++ routes/BotRoutes.js
@@ -5,6 +5,7 @@ import BotController from "../controllers/BotController.js";
 const router = express.Router();
 
 router.get("/list", BotController.listAllBots);
+router.get("/list/:id", BotController.findBotById);
 
 export default router;
 
